0efaedfb5fb037dd242a5bc83cb6abe87ff8a38f
Kristóf Kálai <kristof.kalai@supercharge.io>
2024-01-01 04:08:30 +0100

    feat: improve codebase

--- Sources/AngleGradientView/AngleGradientView.swift
+++ Sources/AngleGradientView/AngleGradientView.swift
@@ -9,16 +9,15 @@ import UIKit
 import struct SwiftUI.Angle
 
 public final class AngleGradientView: UIView {
-    private class GradientLayer: CAGradientLayer {
-        @NSManaged private var angleInDegrees: CGFloat
-        private var context: CABasicAnimation?
-        fileprivate var angleDidUpdateHook: ((Angle) -> Void)?
+    final private class GradientLayer: CAGradientLayer {
+        @NSManaged fileprivate var angleInDegrees: CGFloat
+        fileprivate var angleDidUpdate: ((Angle) -> Void)?
 
         override init(layer: Any) {
             super.init(layer: layer)
-            if let layer = layer as? GradientLayer {
+            if let layer = layer as? Self {
                 angleInDegrees = layer.angleInDegrees
-                angleDidUpdateHook = layer.angleDidUpdateHook
+                angleDidUpdate = layer.angleDidUpdate
             }
         }
 
@@ -34,101 +33,80 @@ public final class AngleGradientView: UIView {
             key == #keyPath(angleInDegrees) ? true : super.needsDisplay(forKey: key)
         }
 
-        override class func defaultAction(forKey event: String) -> CAAction? {
-            // disable implicit animations
-            [#keyPath(angleInDegrees), #keyPath(startPoint), #keyPath(endPoint)].contains(event) ? NSNull() : CAGradientLayer.defaultAction(forKey: event)
-        }
-
         override func action(forKey event: String) -> CAAction? {
-            guard event == #keyPath(angleInDegrees) else {
+            switch event {
+            case #keyPath(angleInDegrees):
+                let context = action(forKey: #keyPath(backgroundColor)) as? CABasicAnimation
+                guard let animation = context?.copy() as? CABasicAnimation else { return nil }
+                animation.keyPath = event
+                animation.fromValue = presentation()?.value(forKeyPath: event)
+                animation.toValue = nil
+                return animation
+            default:
                 return super.action(forKey: event)
             }
-            guard let animation = context?.copy() as? CABasicAnimation else {
-                return NSNull()
-            }
-            CATransaction.commit {
-                animation.keyPath = event
-                animation.fromValue = currentLayer.angleInDegrees
-                animation.toValue = nil
-            } completion: { [weak self] in
-                self?.context = nil
-            }
-            return animation
         }
 
         override func draw(in ctx: CGContext) {
             super.draw(in: ctx)
-            apply(angle: currentLayer.angleInDegrees, on: model())
-            angleDidUpdateHook?(.degrees(currentLayer.angleInDegrees))
-        }
-
-        fileprivate func set(angle: CGFloat, with context: CAAction?) {
-            self.context = (context as? CABasicAnimation)?.copy() as? CABasicAnimation
-            angleInDegrees = angle
+            apply(degrees: currentLayer.angleInDegrees, on: model())
+            angleDidUpdate?(.degrees(currentLayer.angleInDegrees))
         }
     }
 
-    public var colors: [UIColor] = [] {
-        didSet {
-            updateGradient()
-        }
-    }
-    public var locations: [CGFloat]? = nil {
-        didSet {
-            updateGradient()
-        }
-    }
-    public var angle: Angle = .zero /* .zero means top to bottom gradient */ {
-        didSet {
-            updateGradient()
-        }
-    }
-    public var angleDidUpdateHook: ((Angle) -> Void)? {
+    public var colors: [UIColor]? {
         get {
-            gradientLayer.angleDidUpdateHook
+            _layer.color
         }
         set {
-            gradientLayer.angleDidUpdateHook = newValue
+            _layer.color = newValue
         }
     }
 
-    private let gradientLayer = GradientLayer()
+    public var locations: [CGFloat]? {
+        get {
+            _layer.location
+        }
+        set {
+            _layer.location = newValue
+        }
+    }
 
-    public init(colors: [UIColor] = [], locations: [CGFloat]? = nil, angle: Angle = .degrees(.zero), frame: CGRect = .zero, angleDidUpdateHook: ((Angle) -> Void)? = nil) {
+    public var angle: Angle /* .zero means top to bottom gradient */ {
+        get {
+            .init(degrees: _layer.angleInDegrees)
+        }
+        set {
+            _layer.angleInDegrees = .init(newValue.degrees)
+        }
+    }
+
+    public var angleDidUpdate: ((Angle) -> Void)? {
+        get {
+            _layer.angleDidUpdate
+        }
+        set {
+            _layer.angleDidUpdate = newValue
+        }
+    }
+
+    private var _layer: GradientLayer {
+        layer as! GradientLayer
+    }
+
+    public override class var layerClass: AnyClass {
+        GradientLayer.self
+    }
+
+    public init(frame: CGRect = .zero, colors: [UIColor]? = nil, locations: [CGFloat]? = nil, angle: Angle = .zero, angleDidUpdate: ((Angle) -> Void)? = nil) {
+        super.init(frame: frame)
         self.colors = colors
         self.locations = locations
         self.angle = angle
-        super.init(frame: frame)
-        self.angleDidUpdateHook = angleDidUpdateHook
-
-        setupView()
+        self.angleDidUpdate = angleDidUpdate
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupView()
-    }
-}
-
-extension AngleGradientView {
-    private func setupView() {
-        layer.addSublayer(gradientLayer)
-        updateGradient()
-    }
-}
-
-extension AngleGradientView {
-    public override var bounds: CGRect {
-        didSet {
-            gradientLayer.frame = bounds
-        }
-    }
-}
-
-extension AngleGradientView {
-    private func updateGradient() {
-        gradientLayer.color = colors
-        gradientLayer.location = locations
-        gradientLayer.set(angle: angle.degrees, with: action(for: gradientLayer, forKey: #keyPath(backgroundColor)))
     }
 }
